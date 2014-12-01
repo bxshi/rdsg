a80998fcca2a534687843e53a4e6dc0e00239d58
bxshi <baoxu.shi@gmail.com>
2014-12-01 00:26:16 -0500

    add example to dsg_color, reorder color

--- R/dsg_color.R
+++ R/dsg_color.R
@@ -2,8 +2,8 @@ library(grDevices)
 
 # Since there is no significant difference between photocopy_safe and color_blind_safe when
 # you print it out using a grey-scale printer, I set .color_blind_safe_color_scheme as default
-.photocopy_safe_color_scheme =  c("#D7191C", "#2B83BA", "#FDAE61", "#FFFFBF", "#ABDDA4")
-.color_blind_safe_color_scheme = c("#D7191C", "#2C7BB6", "#FDAE61", "#FFFFBF", "#ABD9E9")
+.photocopy_safe_color_scheme =  c("#D7191C", "#FDAE61", "#FFFFBF", "#ABDDA4", "#2B83BA")
+.color_blind_safe_color_scheme = c("#D7191C", "#FDAE61", "#FFFFBF", "#ABD9E9", "#2C7BB6")
 
 #' Return color schemes. 
 #' 
@@ -12,6 +12,11 @@ library(grDevices)
 #' @param measurevar Use this to measure unique values in data frame to determine the number of colors we return
 #' @param num Number of colors you want, if specified, then data and measurevar are ignored 
 #' @param prefer Preference of colors, color_blind_safe or photocopy_safe
+#' 
+#' @examples
+#' dsg_color(num=1)
+#' dsg_color(data=data.frame(x=c(1,2), y=c(3,4), z=c(5,6)), measurevar="z")
+#' 
 #' @export
 dsg_color <- function(data=NULL, measurevar=NULL, num=NULL, prefer="color_blind_safe") {
   if(prefer == "color_blind_safe") {
