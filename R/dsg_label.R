8830e5603654312c253fbd7ff6bf2567c906b795
bxshi <baoxu.shi@gmail.com>
2014-12-01 00:26:00 -0500

    export label_function w/ example

--- R/dsg_label.R
+++ R/dsg_label.R
@@ -1,13 +1,22 @@
 #' Generate complex label for plot
 #'
-#' Currently this can only take one parameter
+#' TODO: Make formula `...` more easy to write. 
 #' 
-#' @param x A list of data you want to use to construct the label
+#' @param x A list/datafarme you want use to construct the label
 #' @param formula Formula passing to bquote, usually a paste function
 #' 
+#' @examples
 #' data <- c(1,2,3,4,5)
-#' label_function(data, paste(gamma, "=", .(x))
+#' label_function(data, paste(gamma, "=", .(x)))
+#' dat <- data.frame(x=c(1,2), y=c(3,4), z=c(5,6))
+#' label_function(dat, paste(alpha,"=", .(x[1]), beta, "=", .(x[2]), gamma, "=",.(x[3])))
+#' 
 #' @export
 label_function <- function(x, ...) {
-  return(lapply(x, function(x) bquote(...)))
+  if(is.data.frame(x)) {
+    return(apply(x, 1, function(x) bquote(...)))
+  } else {
+    # Not data.frame, use old methods, this only support 1 parameter
+    return(lapply(x, function(x) bquote(...)))
+  }
 }
