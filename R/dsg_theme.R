f7bec2b4df2c3345ccb755d8deaabaf54808e106
bxshi <baoxu.shi@gmail.com>
2014-12-08 13:17:58 -0500

    Add border to plot, enlarge font size

--- R/dsg_theme.R
+++ R/dsg_theme.R
@@ -22,13 +22,15 @@ dsg_theme <- function(font_size = 12) {
                   # No background
                   panel.background = element_blank(),
                   # Text settings for scale
-                  #axis.text = element_text(size=16),
+                  axis.text = element_text(size=16),
                   # Text settings for axis titles
-                  #axis.title.y = element_text(size=16),
-                  #axis.title.x = element_text(size=18),
+                  axis.title.y = element_text(size=18),
+                  axis.title.x = element_text(size=18),
                   # Legend position
                   legend.position = "bottom",
-                  #legend.text = element_text(size=16),
+                  legend.text = element_text(size=16),
                   # No legend title
-                  legend.title = element_blank()))
+                  legend.title = element_blank(),
+                  # Border
+                  panel.border = element_rect(colour = "black", fill=NA)))
 }
